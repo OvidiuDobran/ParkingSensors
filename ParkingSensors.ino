aa85045fb31fae9ff0850e530f21a2724a060814
unknown <dobran_ovi@yahoo.com>
2018-03-17 15:19:52 +0200

    first commit

---
+++ ParkingSensors.ino
@@ -0,0 +1,81 @@
+
+long minDist=7;
+int buzzer=12;
+
+struct distSensor {
+  int trigPin;
+  int echoPin;
+};
+
+struct distSensor ds[4];
+
+void setup() {
+  // put your setup code here, to run once:
+  ds[0].trigPin=10;
+  ds[0].echoPin=11;
+  ds[1].trigPin=8;
+  ds[1].echoPin=9;
+  ds[2].trigPin=6;
+  ds[2].echoPin=7;
+  ds[3].trigPin=4;
+  ds[3].echoPin=5;
+  
+  for (int i=0;i<4;i++){
+    pinMode(ds[i].trigPin, OUTPUT); // Sets the trigPin as an Output
+    pinMode(ds[i].echoPin, INPUT); // Sets the echoPin as an Input
+  }
+  Serial.begin(9600); // Starts the serial communication
+}
+
+void loop() {
+  // put your main code here, to run repeatedly:
+  
+  // Prints the distance on the Serial Monitor
+  for(int i=0;i<4;i++){
+    Serial.print("Distance sensor ");Serial.print(i);Serial.print(": ");
+    Serial.println(getDistanceForSensor(ds[i]));
+  }
+  Serial.println();
+  if (isTooClose())
+  {
+    digitalWrite(buzzer, HIGH);
+  }
+  else
+  {
+    digitalWrite(buzzer, LOW);
+  }
+  delay(500);
+}
+
+long getDistanceForSensor(distSensor ds)
+{
+  long duration;
+  int distance;
+  // Clears the trigPin
+  digitalWrite(ds.trigPin, LOW);
+  delayMicroseconds(2);
+  // Sets the trigPin on HIGH state for 10 micro seconds
+  digitalWrite(ds.trigPin, HIGH);
+  delayMicroseconds(10);
+  digitalWrite(ds.trigPin, LOW);
+  // Reads the echoPin, returns the sound wave travel time in microseconds
+  duration = pulseIn(ds.echoPin, HIGH);
+  // Calculating the distance
+  distance= duration*0.034/2;
+  return distance;
+}
+
+
+int isTooClose()
+{
+  for (int i=0;i<4;i++)
+  {
+    if(getDistanceForSensor(ds[i])<=minDist)
+    {
+      return 1;  
+    }
+  }
+  return 0;
+}
+
+
